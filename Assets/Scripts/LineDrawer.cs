f54700c83c3cae92ee8ab9c997b0a36656a15ed1
Vit <banduravit@gmail.com>
2022-03-27 11:35:12 +0300

    Imported models and animations. Totally reworked drawing system

--- Assets/Scripts/LineDrawer.cs
+++ Assets/Scripts/LineDrawer.cs
@@ -1,53 +1,78 @@
 using UnityEngine;
 
-public class LineDrawer : MonoBehaviour
+public class LineDrawer : MonoBehaviour //, IPointerEnterHandler, IPointerExitHandler
 {
-   public const float RESOLUTION = 0.2f;
+    private const float ZAxisOffset = 10f;
+    private const float Resolution = 0.2f;
+
+    [SerializeField] private GameObject _linePrefab;
+ 
+    private Camera _camera;
+    private GameObject _line;
+    private LineRenderer _lineRenderer;
+    private Vector3 _mousePosition;
+    private Vector3 _newDrawingPoint;
     
-   private Camera _camera;
-   [SerializeField] private GameObject _linePrefab;
-   
-   private GameObject _currentLine;
-   private Line _lineComponentInCurrentLine;
-   
-   private void Start()
-   {
-       _camera = Camera.main;
-       InitializeLine();
-   }
+    
+    private void Awake()
+    {
+        InitializeCamera();
+        InitializeLine();
+    }
 
-   private void Update()
-   {
-       Vector2 mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+    private void Update()
+    {
        
-       if (Input.GetMouseButtonDown(0))
-       {
-           StartDrawingLine(mousePosition);
-       }
-      
-       if (Input.GetMouseButton(0))
-       {
-           KeepDrawingLine(mousePosition);
-       }
-   }
-   
-   private void InitializeLine()
-   {
-       _currentLine = Instantiate(_linePrefab);
-       _lineComponentInCurrentLine = _currentLine.GetComponent<Line>();
-   }
-   
-   private void StartDrawingLine(Vector2 mousePosition)
-   {
-       _lineComponentInCurrentLine.RefreshLine();
-       _currentLine.transform.position = mousePosition;
-   }
-  
-   private void KeepDrawingLine(Vector2 mousePosition)
-   {
-       _lineComponentInCurrentLine.SetPosition(mousePosition);
-   }
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartDrawingNewLine();
+        }
+        
+        if (Input.GetMouseButton(0))
+        {
+            KeepDrawingNewLine();
+        }
+    }
 
-   
-   
+    private void InitializeCamera()
+    {
+        _camera = Camera.main;
+    }
+    
+    private void InitializeLine()
+    {
+        _line = Instantiate(_linePrefab);
+        _lineRenderer = _line.GetComponent<LineRenderer>();
+    }
+    
+    private void StartDrawingNewLine()
+    {
+        _lineRenderer.positionCount = 0;
+    }
+    
+    private void KeepDrawingNewLine()
+    {
+        UpdatePointerPosition();
+        _newDrawingPoint = _camera.ScreenToWorldPoint(_mousePosition);
+        if (CanAppendPosition(_newDrawingPoint))
+        {
+            _lineRenderer.positionCount++;
+            _lineRenderer.SetPosition(_lineRenderer.positionCount - 1, _newDrawingPoint);
+        }
+    }
+    
+    private void UpdatePointerPosition()
+    {
+        _mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, ZAxisOffset);
+    }
+    
+    private bool CanAppendPosition(Vector3 position)
+    {
+        if (_lineRenderer.positionCount == 0)
+        {
+            return true;
+        }
+        return Vector3.Distance(_lineRenderer.GetPosition(_lineRenderer.positionCount - 1), position)
+               > Resolution;
+    }
 }
